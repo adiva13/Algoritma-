c26a0ad28c1026b0e42279392ddd7fb93ccaaacd
adiva13 <sitiadivakirani05majene@gmail.com>
2025-12-11 07:18:27 +0800

    Add files via upload

---
+++ TipeData_array_strukturUnions_variabelSttis&dnms/1.3_konsep_stuktur.py
@@ -0,0 +1,14 @@
+#Struktur adalah kumpulan beberapa data yang berbeda-beda tipe, tetapi digabung menjadi satu kesatuan. Struktur digunakan ketika kita ingin menyimpan data yang berhubungan dalam satu paket.
+
+#data_kendaraan
+kendaraan = {
+    "merk": "Yamaha",
+    "tahun": 2020,
+    "warna": "Hitam"
+}
+
+print("Merk:", kendaraan["merk"])
+print("Tahun:", kendaraan["tahun"])
+print("Warna:", kendaraan["warna"])
+
+#Struktur ini menyimpan data kendaraan dalam satu wadah.
